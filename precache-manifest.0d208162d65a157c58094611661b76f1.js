6427101f5325529fd31582b75499de8462ffc011
Kosala Mudalige <mudaligekosala@gmail.com>
2018-11-24 15:14:42 +1100

    Updates

---
+++ precache-manifest.0d208162d65a157c58094611661b76f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/servo_web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fcb964ee3d1627587ea7",
+    "url": "/servo_web/static/js/runtime~main.fcb964ee.js"
+  },
+  {
+    "revision": "40169fb7a4aa7e0c2c47",
+    "url": "/servo_web/static/js/main.40169fb7.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/servo_web/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "40169fb7a4aa7e0c2c47",
+    "url": "/servo_web/static/css/main.865fbecd.chunk.css"
+  },
+  {
+    "revision": "1740acf8438b8f17e7d3cd0223198deb",
+    "url": "/servo_web/index.html"
+  }
+];
